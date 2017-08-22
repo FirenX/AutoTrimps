2bf4fc3e5df750cb7918b0249f0ace9ae638a08e
FirenX <15254737+FirenX@users.noreply.github.com>
2017-08-22 02:18:51 +0200

    additional url fixes

--- user.js
+++ user.js
@@ -1,9 +1,9 @@
 // ==UserScript==
-// @name         AutoTrimps-genBTC
-// @namespace    https://github.com/genbtc/AutoTrimps
-// @version      2.1.5.2-genbtc-12-23-2016+Modular
+// @name         AutoTrimps-FirenX
+// @namespace    https://github.com/FirenX/AutoTrimps
+// @version      3.0.0.0-FirenX-08-21-2017
 // @description  Automate all the trimps!
-// @author       zininzinin, spindrjr, Ishkaru, genBTC
+// @author       zininzinin, spindrjr, Ishkaru, genBTC, FirenX
 // @include        *trimps.github.io*
 // @include        *kongregate.com/games/GreenSatellite/trimps
 // @grant        none
@@ -11,7 +11,7 @@
 
 var script = document.createElement('script');
 script.id = 'AutoTrimps-script';
-script.src = 'https://genbtc.github.io/AutoTrimps/AutoTrimps2.js';
+script.src = 'https://FirenX.github.io/AutoTrimps/AutoTrimps2.js';
 document.head.appendChild(script);
 
 // This File is Deprecated, please use .user.js instead (with the leading dot) so TamperMonkey/Greasemonkey can detect it as a script automatically.
