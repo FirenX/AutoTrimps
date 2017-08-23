b3863c7cd9be5f0445c1dab43896c869d06defe2
FirenX <15254737+FirenX@users.noreply.github.com>
2017-08-23 15:17:21 +0200

    fix

--- AutoTrimps2.js
+++ AutoTrimps2.js
@@ -220,7 +220,7 @@ function mainLoop() {
 //GUI Updates happen on this thread, every 1000ms, concurrently
 function guiLoop() {
     updateCustomButtons();
-    setTimeout(guiLoop, runInterval*100);
+    setTimeout(guiLoop, runInterval*10);
 }
 
 // Userscript loader. write your own!
